435db56806c740a210020dde128a957daefb3700
theophilus <theophilus3031@gmail.com>
2019-04-04 11:44:27 +0300

    fetched add sources related to technology

--- app/src/main/java/theo/tech/instanews/app/Fragments/TechFragment.kt
+++ app/src/main/java/theo/tech/instanews/app/Fragments/TechFragment.kt
@@ -5,15 +5,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import theo.tech.instanews.Entinties.Article
+import theo.tech.instanews.Entinties.Category
+import theo.tech.instanews.Network.ApiClient
 import theo.tech.instanews.R
+import theo.tech.instanews.app.Adapters.AllArticlesAdapter
+import theo.tech.instanews.app.Adapters.SourcesAdapter
 
 /**
  * created by theop
  * on 4/3/2019 at 10:22 PM
  */
-class TechFragment:Fragment() {
+class TechFragment:Fragment(),SourcesAdapter.Listener {
+
+    val client by lazy {
+        ApiClient.create()
+    }
+    private lateinit var rv_content_list: RecyclerView
+    lateinit var compositeDisposable: CompositeDisposable
+    private lateinit var sourcesLists:ArrayList<Category>
+    private var sourcesAdapter: SourcesAdapter?=null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
        val rootView:View=inflater.inflate(R.layout.tech_main_layout,container,false)
+       compositeDisposable= CompositeDisposable()
+        rv_content_list=rootView.findViewById(R.id.rv_content_list)
+        initViews()
+        fetchSources()
+
         return rootView
     }
+
+    private fun initViews() {
+        rv_content_list.setHasFixedSize(true)
+        val layoutManager:RecyclerView.LayoutManager= LinearLayoutManager(context)
+        rv_content_list.layoutManager=layoutManager
+
+    }
+/*fetch all sources for category tech related*/
+    private fun fetchSources() {
+    compositeDisposable.add(client.getSourceTechCategory(
+        "technology"
+    )
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribe({category->handleResponce(category.sources)},
+            this::handleError))
+    }
+
+    private fun handleResponce(sources: List<Category>) {
+        sourcesLists= ArrayList(sources)
+        sourcesAdapter= SourcesAdapter(this.context!!,sourcesLists,this)
+        rv_content_list.adapter=sourcesAdapter
+    }
+
+    private fun handleError(e:Throwable){
+        e.printStackTrace()
+    }
+
+    override fun onSourceClick(category: Category) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
